26486187057c7b5cea37bfdb94d9464924aee7a9
AbimaelFranco <3006701680101@ingenieria.usac.edu.gt>
2024-09-07 11:20:51 -0600

    uso de templates y adición de shop

--- main/views.py
+++ main/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 def Home(request):
 
-    return render(request, "index.html")
+    return render(request, "home.html")
+
+def Shop(request):
+
+    return render(request, "shop.html")
